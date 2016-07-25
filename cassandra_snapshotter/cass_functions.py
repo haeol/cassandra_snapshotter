e1c4e44ff8e544a0c285fed9be07faab6838a565
HaeoL <k.tom_1@yahoo.com>
2016-07-25 14:47:09 -0700

    first commit

---
+++ cassandra_snapshotter/cass_functions.py
@@ -0,0 +1,107 @@
+import yaml
+import os
+import subprocess
+
+def cassandra_query(query, output=True):
+
+    if type(query) is str:
+        query = ['echo', query]
+    elif type(query) is not list: # TODO needed?
+        raise Exception('Query not recognized')
+
+    query_process = subprocess.Popen(query, stdout=subprocess.PIPE)
+    cqlsh = subprocess.Popen(('/bin/cqlsh'),
+                             stdin=query_process.stdout,
+                             stdout=subprocess.PIPE)
+    query_process.stdout.close()
+
+    if output:
+
+        output = cqlsh.communicate()[0]
+        query_process.wait()
+        return output
+
+
+def get_data_dir():
+
+    install_locations = ['/etc/cassandra/conf/', # package install
+                         '/etc/dse/cassandra/'  # datastax enterprise package
+                         ] #TODO tarball install needs install location
+
+    for loc in install_locations:
+        if os.path.exists(loc + 'cassandra.yaml'):
+            yaml_dir = loc + 'cassandra.yaml'
+            break
+    else:
+        # TODO user manual yaml location input through txt(?) file
+        raise Exception('Could not find cassandra YAML file.')
+
+    with open(yaml_dir, 'r') as f:
+        cass_yaml = yaml.load(f)
+    return cass_yaml['data_file_directories'][0]
+
+
+def get_keyspaces(system=False): # include system keyspaces?
+    _SYSTEM_KEYSPACES = set(['system_schema',
+                             'system_auth',
+                             'system',
+                             'system_distributed',
+                             'system_traces'])
+
+    keyspaces_string = cassandra_query('DESCRIBE keyspaces;')
+    keyspaces = set(keyspaces_string.strip().split())
+    if not system:
+        keyspaces = keyspaces - _SYSTEM_KEYSPACES
+    return keyspaces
+
+def get_table_directories(keyspace):
+
+    cmd = ("SELECT table_name, id FROM system_schema.tables \
+            WHERE keyspace_name='%s';" % keyspace)
+    query = cassandra_query(cmd).split('\n')
+
+    # format of query is as follows, may need to be updated
+    '''
+
+    table_name  | id
+    ------------+---------------------------
+    first_table | first_uuid
+    . . .       | . . .
+    last_table  | last_uuid
+
+    (num rows)
+    '''
+
+    table_directory = {}
+
+    query = query[3:-3] #TODO check if before and after match regex?
+
+    for row in query:
+
+        table, uuid = row.strip().split(' | ')
+        uuid = uuid.replace('-', '')
+        table_directory[table] = table + '-' + uuid
+
+    return table_directory
+
+
+def get_dir_structure(keyspaces):
+
+    '''
+    structure: {
+        keyspace1 : {
+            'table1' : 'directory1',
+            'table2' : 'directory2',
+            . . .
+        },
+        keyspace2 : {
+            . . .
+        }
+    }
+    '''
+
+    structure = {}
+    for keyspace in keyspaces:
+        structure[keyspace] = get_table_directories(keyspace)
+
+    return structure
---
+++ cassandra_snapshotter/restore.py
@@ -0,0 +1,213 @@
+from __future__ import absolute_import
+
+import os
+import sys
+import argparse
+import subprocess
+import shutil
+import time
+
+from cass_functions import (cassandra_query, get_data_dir, get_keyspaces,
+                            get_table_directories, get_dir_structure)
+
+
+def parse_cmd():
+
+    parser = argparse.ArgumentParser(description='Snapshotter')
+
+    parser.add_argument('-d', '--path', 
+                        type=check_dir, 
+                        required=True,
+                        help="Specify path to load snapshots"
+    )
+    parser.add_argument('-n', '--node', '--host',
+                        required=True,
+                        nargs='+',
+                        help="Specify host address(es)"
+    ) # TODO need to test on multinode cluster, necessary?
+      # script running on single nodes or data center?
+    parser.add_argument('-k', '-ks', '--keyspace',
+                        required=False,
+                        nargs='+',
+                        help="Specify keyspace(s)"
+    )
+    parser.add_argument('-tb', '-t', '--table', '-cf', '--column_family',
+                        required=False,
+                        nargs='+',
+                        help="Specify table(s)"
+    )
+    parser.add_argument('-y',
+                        required=False,
+                        action='store_true',
+                        help="Destroy existing database without prompt"
+    )
+    # TODO sstableloader option? safer loading?
+
+    return parser.parse_args()
+
+
+def check_dir(folder):
+
+    if not os.path.isdir(folder):
+        raise argparse.ArgumentTypeError('Directory does not exist')
+    if os.access(folder, os.R_OK):
+        return folder
+    else:
+        raise argparse.ArgumentTypeError('Directory is not readable')
+
+
+def restore_schema(load_path, keyspace):
+
+    schema_location = load_path + '/' + keyspace + '/' + keyspace + '_schema.cql'
+
+    if not os.path.exists(schema_location):
+        raise Exception('Schema not found: %s' % schema_location)
+
+    with open(schema_location, 'r') as f:
+        cassandra_query(f.read())
+
+
+def clean_directory(table_directory):
+    # TODO does incremental backups work with this?
+    for f in os.listdir(table_directory):
+        if f.endswith('.db') or f.endswith('.txt') or f.endswith('.crc32'):
+            os.remove(table_directory + '/' + f)
+
+
+def destroy_schema(flag=None):
+
+    success = False
+    destroy = False
+    keyspaces = get_keyspaces()
+
+    if len(keyspaces) > 0:
+
+        print('Removing keyspaces:')
+        for k in keyspaces:
+            print('\t' + k)
+        if not flag:
+
+            option = raw_input('Destroy keyspaces? [y/n]')
+            if option == 'y' or option == 'Y':
+                destroy = True
+
+        elif flag == '-y':
+            destroy = True
+
+        else: # should never happen
+            raise Exception('Invalid flag parameter')
+
+        if destroy:
+
+            for k in keyspaces: # drop old keyspaces
+                print('Dropping keyspace: %s' % k)
+                cassandra_query('DROP KEYSPACE %s;' % k)
+
+            data_dir = get_data_dir()
+            active_dirs = os.listdirs(data_dir)
+
+            print('Removing old keyspace directories')
+            for d in active_dirs:
+                if d in keyspaces:
+                    print('Removing keyspace directory: %s/%s' % (data_dir, d))
+                    shutil.rmtree(data_dir + '/' + d)
+                
+            success = True
+
+    else:
+        success = True
+
+    return success
+
+
+def restore(load_path, hosts, keyspace_arg = None, table_arg = None,
+            y_flag=None):
+
+    print('Destroying existing database')
+    if not destroy_schema(y_flag):
+        print('Unable to destroy previous data, exiting script')
+        sys.exit(0)
+
+    # keyspaces inside snapshot directory
+    avaliable_keyspaces = filter(lambda x: os.path.isdir(load_path + '/' + x), \
+                                 os.listdir(load_path))
+
+    print('Checking keyspace arguments')
+    if keyspace_arg:
+
+        for keyspace in keyspace_arg:
+            if keyspace not in avaliable_keyspaces:
+                raise Exception('Keyspace "%s" not in snapshot folder' % keyspace)
+        load_keyspaces = keyspace_arg
+
+    else:
+        load_keyspaces = avaliable_keyspaces
+
+    for keyspace in load_keyspaces:
+        print('Creating schema for %s' % keyspace)
+        restore_schema(load_path, keyspace)
+
+    existing_keyspaces = get_keyspaces()
+    structure = get_dir_structure(existing_keyspaces)
+    cass_data_dir = get_data_dir()
+
+    for keyspace in load_keyspaces:
+
+        print('Loading keyspaces %s' % keyspace)
+        load_tables = filter(
+                lambda x: os.path.isdir(load_path + '/' + keyspace + '/' + x),
+                os.listdir(load_path + '/' + keyspace)
+        )
+        existing_tables = structure[keyspace].keys()
+
+        for table in load_tables:
+
+            if table not in existing_tables:
+                raise Exception('Table not in schema, error with snapshot')
+
+            load_table_dir = load_path + '/' + keyspace + '/' + table
+            data_table_dir = cass_data_dir + '/' + keyspace + '/' + \
+                             structure[keyspace][table]
+
+            print('Checking for old data files')
+            clean_directory(data_table_dir)
+
+            for f in os.listdir(load_table_dir):
+                if f.endswith('.db') or f.endswith('.txt.'):
+                    shutil.copy(load_table_dir + '/' + f, data_table_dir)
+            
+            print('Loading table: %s' % table)
+            # sstablelaoder is slower for smaller tables, but more stable
+            subprocess.call(['/bin/sstableloader', '-d', hosts, load_table_dir])
+            #TODO determine which to use
+            #TODO still needs to be tested on multi-node cluster
+            '''
+            for h in hosts.split(', '):
+                subprocess.call(['/bin/nodetool', '-h', h, 'refresh',
+                                 '--', keyspace, table])
+            '''
+            print('Cleaning data directory: %s' % data_table_dir)
+            clean_directory(data_table_dir)
+
+            print('Restoration complete')
+
+
+if __name__ == '__main__':
+
+    cmds = parse_cmd()
+    if cmds.path.endswith('\\') or cmds.path.endswith('/'):
+        load_path = cmds.path[:-1]
+    else:
+        load_path = cmds.path
+
+    nodes = ', '.join(cmds.node)
+
+    start = time.time()
+    restore(load_path, nodes, cmds.keyspace, cmds.table, cmds.y)
+    end = time.time()
+
+    print('Elapsed time: %s' % (end - start))
+
+
+
+
