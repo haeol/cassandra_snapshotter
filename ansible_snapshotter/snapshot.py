d734ccd53486c4b1f893e335b1cad62b5e3a8b7f
Kevin Tom <k.tom_1@yahoo.com>
2016-08-09 17:20:16 -0400

    working with temp folder

--- ansible_snapshotter/snapshot.py
+++ ansible_snapshotter/snapshot.py
@@ -16,7 +16,7 @@ def parse_cmd():
                         help='Specify a path to the snapshot zip file'
     )
     parser.add_argument('-n', '--nodes', '--hosts',
-                        required=False,
+                        required=True,
                         help='Enter the host group from the inventory'
     )
     parser.add_argument('-k', '-ks', '--keyspace',
@@ -46,6 +46,27 @@ def check_dir(folder):
         raise argparse.ArgumentTypeError('Directory is not readable')
 
 
+def zipdir(root_path, save_path, title):
+
+    zipf = zipfile.ZipFile(save_path + '/' + title + '.zip',
+                           'w', zipfile.ZIP_DEFLATED)
+    rootlength = len(root_path)
+    for root, dirs, files in os.walk(root_path):
+        for f in files:
+            filename = os.path.join(root_path, f)
+            zipf.write(filename, filename[rootlength:])
+
+
+def make_directory(path):
+
+    exists = False
+    if not os.path.isdir(path):
+        os.makedirs(path)
+    else:
+        exists = True
+    return exists
+
+
 def ansible_snapshot(cmds):
 
     if cmds['title']:
@@ -61,24 +82,33 @@ def ansible_snapshot(cmds):
         if not os.path.isdir(save_path):
             os.makedirs(save_path)
 
-    save_path = save_path + '/' + title
-    if os.path.isdir(save_path):
-        raise Exception('%s has already been created' % save_path)
-    else:
-        os.makedirs(save_path)
-    
-    if not os.path.isdir(sys.path[0] + '/output_logs'):
-        os.makedirs(sys.path[0] + '/output_logs')
-    
+    temp_path = sys.path[0] + '/.temp'
+    zipfile = save_path + '/' + title + '.zip'
+    if os.path.isfile(zipfile):
+        raise Exception('%s has already been created' % zipfile)
+
+    make_directory(save_path)
+    make_directory(sys.path[0] + '/output_logs')
+    if make_directory(temp_path):
+        for f in os.listdir(temp_path):
+            if os.path.isdir(temp_path + '/' + f):
+                shutil.rmtree(temp_path + '/' + f)
+            else:
+                os.remove(temp_path + '/' + f)
+    os.makedirs(temp_path + '/' + title)
+
     playbook_args = dict((key, value) for key, value in cmds.iteritems()
                         if value != None and key != 'path' and key != 'title')
-    playbook_args['path'] = save_path
+    playbook_args['path'] = temp_path + '/' + title
 
     return_code = run_playbook('snapshot.yml', playbook_args)
     if return_code != 0:
-        shutil.rmtree(save_path)
+        shutil.rmtree(temp_path + '/' + title)
     else:
-        shutil.make_archive(save_path, 'zip', save_path)
+        zipdir(temp_path + '/' + title, save_path, title)
+
+    print('Process complete, output logs saved in %s' %
+         (sys.path[0] + '/output_logs'))
 
 
 if __name__ == '__main__':
